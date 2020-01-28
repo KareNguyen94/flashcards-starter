cdf193d9222eeb699dd0d35a9d15af002632edf2
Karen Nguyen <knguyen4184@yahoo.com>
2020-01-28 00:37:01 -0700

    Start game method in game class

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,13 @@
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = null;
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -12,6 +16,14 @@ class Game {
 
   printQuestion(round) {
       util.main(round);
+  }
+
+  startGame() {
+    const deck = new Deck(prototypeQuestions);
+    const round = new Round(deck);
+    this.currentRound = round;
+    this.printMessage(deck, round);
+    this.printQuestion(round);
   }
 }
 
